f3b4dd48b3a062127d825d69cce15481a0aa3d07
Joseph Kinahan <kinahanj25@gmail.com>
2020-07-15 23:10:43 +0100

    first commit/ bare aul bones

---
+++ src/main/java/com/majortomdev/MooTube/MooTubeApplication.java
@@ -0,0 +1,13 @@
+package com.majortomdev.MooTube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooTubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/majortomdev/MooTube/MooTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majortomdev.MooTube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
