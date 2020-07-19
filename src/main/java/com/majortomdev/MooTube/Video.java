ad1e34c36d4b5b9db619df317e95e466fa844857
Joseph Kinahan <kinahanj25@gmail.com>
2020-07-19 11:42:43 +0100

    vamped up video class, inluded setURL into controller

--- src/main/java/com/majortomdev/MooTube/Video.java
+++ src/main/java/com/majortomdev/MooTube/Video.java
@@ -1,14 +1,20 @@
 package com.majortomdev.MooTube;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /*by JoeK 15/07/20*/
 public class Video {
 	
+	private long id;
 	private String title;
-	
-	 
-	private Long id;
+	private long duration;
+	private String location;
+	private String subject;
+	private String contentType;
+
+	@JsonIgnore
+	private String dataUrl;
 
 	public String getTitle() {
 		return title;
@@ -25,5 +31,53 @@ public class Video {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@JsonProperty
+	public String getDataUrl() {
+		return dataUrl;
+	}
+
+	@JsonIgnore
+	public void setDataUrl(String dataUrl) {
+		this.dataUrl = dataUrl;
+	}
+
+
 
 }
--- src/main/java/com/majortomdev/MooTube/VideoController.java
+++ src/main/java/com/majortomdev/MooTube/VideoController.java
@@ -5,21 +5,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class VideoController {
 	
 	private List<Video> videos = new ArrayList<Video>();
+	
+	@GetMapping("/video")
+	public List<Video>  getVideoList() {
+		
+
+		
+		return videos;
+	}
+	
+	
+	
+	
+	
 
 	@PostMapping("/video")
-	public @ResponseBody Video saveVideo(@RequestBody Video vid) {
-		vid.setId(1l);
+	public Video saveVideo(@RequestBody Video vid) {
+		if(videos.size()==0) {
+			vid.setId(1l);
+		}else {
+			long l = videos.get(videos.size()-1).getId();
+			vid.setId(l+1);
+		}
+		
+		vid.setDataUrl("http://localhost:8081/video/"+vid.getId()+"/data");
+		
 		videos.add(vid); 
-		
-		
 		return vid;
 	}
 	
